5d91d35c5fafa76e718f292d2fee5b3b6fb88f97
Flipeador <flipeador@gmail.com>
2026-05-10 17:48:10 -0300

    .

--- src/bot/database/database.js
+++
@@ -1,7 +0,0 @@
-import { DatabaseSync } from 'node:sqlite';
-
-const ROOT = import.meta.dirname;
-
-class DataBase extends DatabaseSync { }
-
-export const database = new DataBase(`${ROOT}/db.sqlite`);
